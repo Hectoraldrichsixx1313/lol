659abff3cb8f3c4a61a7e63a7fdb86b05d527389
Hectoraldrichsixx1313 <49257944+Hectoraldrichsixx1313@users.noreply.github.com>
2019-11-06 23:13:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
-
-// setup ports
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-app.get('/', function(req, res) {
-
-	res.end('Hello YouTube!');
-
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
-// server listens in on port
 app.listen(server_port, server_ip_address, function () {
-	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 

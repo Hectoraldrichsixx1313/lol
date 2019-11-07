06035b3190d18963f25332aa13d82246102ce102
Hectoraldrichsixx1313 <49257944+Hectoraldrichsixx1313@users.noreply.github.com>
2019-11-06 23:44:42 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
+// setup ports
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.get('/', function(req, res) {
+
+	res.end('Hello YouTube!');
+
+});
+
+// server listens in on port
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
 
